3400e455a94b2d963b635e8e742c254c2afc8987
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-02 20:02:10 +0900

    Enemyの基底クラスを制作中

--- Assets/1Ito/Scripts Ito/EnemyBese.cs
+++ Assets/1Ito/Scripts Ito/EnemyBese.cs
@@ -7,8 +7,19 @@ using UnityEngine;
 /// </summary>
 public class EnemyBese : MonoBehaviour
 {
+    [SerializeField] float _enemyHp = default;
+
     public virtual void Attack()
     {
         Debug.LogError("派生クラスでメソッドをオーバライドしてください。");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(_enemyHp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
