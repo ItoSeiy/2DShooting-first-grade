9746527ca882c83065ca7a27305f5afddfa77fb1
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-02 19:38:45 +0900

    wip

--- Assets/1Ito/Scripts Ito/EnemyBese.cs
+++ Assets/1Ito/Scripts Ito/EnemyBese.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Enemy(中ボス,ボス)の基底クラス
+/// </summary>
 public class EnemyBese : MonoBehaviour
 {
-
+    public virtual void Attack()
+    {
+        Debug.LogError("派生クラスでメソッドをオーバライドしてください。");
+    }
 }
