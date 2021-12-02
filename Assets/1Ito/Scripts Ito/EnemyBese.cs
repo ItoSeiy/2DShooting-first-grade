65726fc72eaed41e3bae5a15e9a8e7266a58fef2
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-02 21:04:51 +0900

    敵の基底クラスを制作中

--- Assets/1Ito/Scripts Ito/EnemyBese.cs
+++ Assets/1Ito/Scripts Ito/EnemyBese.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 /// <summary>
 /// Enemy(中ボス,ボス)の基底クラス
 /// </summary>
+[RequireComponent(typeof (Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class EnemyBese : MonoBehaviour
 {
-    [SerializeField] float _enemyHp = default;
+    [SerializeField] public float _enemyHp = default;
 
     public virtual void Attack()
     {
@@ -16,6 +18,11 @@ public class EnemyBese : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.tag == "Bullet")
+        {
+            //var bullet = GetComponent<Bullet>();
+            //_enemyHp -= 
+        }
 
         if(_enemyHp <= 0)
         {
