7e0b1f9d7227c6a23e9f90db37770d3049f8160b
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-04 01:17:30 +0900

    Bulletの基底クラスを制作支柱

---
+++ Assets/1Ito/Scripts Ito/BulletBese.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class BulletBese : MonoBehaviour
+{
+
+}
