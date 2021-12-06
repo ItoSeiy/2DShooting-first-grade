226ed0a4ae8a9074927bfdc033175a0d89f99ec6
神原琉成 <tekunikaru22@gmail.com>
2021-12-06 15:55:12 +0900

    変数名を変更した

--- Assets/2Kanbara/Scripts Kanbara/PlayerBase.cs
+++ Assets/2Kanbara/Scripts Kanbara/PlayerBase.cs
@@ -12,14 +12,14 @@ using System.Threading.Tasks;
 public class PlayerBase : MonoBehaviour
 {
     Rigidbody2D _rb;
-    [SerializeField, Header("動くスピード")] float _movespeed = default;
-    [SerializeField, Header("精密操作のスピード")] float _latemove = default;
+    [SerializeField, Header("動くスピード")] float _moveSpeed = default;
+    [SerializeField, Header("精密操作のスピード")] float _lateMove = default;
     [SerializeField, Header("弾を発射するポジション")] Transform _muzzle = default;
     [SerializeField, Header("弾")] GameObject _bullet = default;
-    [SerializeField, Header("残機")] int _playerlife = default;
-    int[] _bulletindex = new int[] { 0, 1, 2 };
-    bool _bulletstop = default;
-    bool _latemode = default;
+    [SerializeField, Header("残機")] int _playerLife = default;
+    int[] _bulletIndex = new int[] { 0, 1, 2 };
+    bool _bulletStop = default;
+    bool _lateMode = default;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -32,28 +32,28 @@ public class PlayerBase : MonoBehaviour
         if (Input.GetButton("Fire3"))
         {
             Vector2 go = new Vector2(h, v).normalized;
-            _rb.velocity = go * _latemove;
-            _latemode = true;
+            _rb.velocity = go * _lateMove;
+            _lateMode = true;
         }
         else
         {
             Vector2 go = new Vector2(h, v).normalized;
-            _rb.velocity = go * _movespeed;
-            _latemode = false;
+            _rb.velocity = go * _moveSpeed;
+            _lateMode = false;
         }
 
-        if (Input.GetButton("Fire1") && _latemode && !_bulletstop)
+        if (Input.GetButton("Fire1") && _lateMode && !_bulletStop)
         {
             PlayerSuperAttack();
-            _bulletstop = true;
+            _bulletStop = true;
         }
-        else if (Input.GetButton("Fire1") && !_bulletstop)
+        else if (Input.GetButton("Fire1") && !_bulletStop)
         {
             PlayerAttack();
-            _bulletstop = true;
+            _bulletStop = true;
         }
 
-        if(_playerlife == 0)
+        if(_playerLife == 0)
         {
             Destroy(this.gameObject);
         }
@@ -64,7 +64,7 @@ public class PlayerBase : MonoBehaviour
         Debug.LogError("派生クラスでメソッドをオーバライドしてください。");
         //GameObject gob = Instantiate(_bullet, _muzzle); //テスト用
         await Task.Delay(100);
-        _bulletstop = false;
+        _bulletStop = false;
     }
 
     public virtual async void PlayerSuperAttack()
@@ -72,14 +72,14 @@ public class PlayerBase : MonoBehaviour
         Debug.LogError("派生クラスでメソッドをオーバライドしてください。");
         //GameObject gob = Instantiate(_bullet, _muzzle); //テスト用
         await Task.Delay(200);
-        _bulletstop = false;
+        _bulletStop = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
-            _playerlife -= 1;
+            _playerLife -= 1;
         }
     }
 }
