91f90b0cc435ab2a72e7507ce04b7746db4b41e7
神原琉成 <tekunikaru22@gmail.com>
2021-12-07 00:20:15 +0900

    メタファイルのコミット漏れ

---
+++ Assets/2Kanbara/Scripts Kanbara/Player2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class Player2 : PlayerBase
+{
+    public override async void PlayerAttack()
+    {
+        //base.PlayerAttack();
+        GameObject gob = Instantiate(_bullet, _muzzle);
+        await Task.Delay(_attackDelay);
+        _bulletStop = false;
+    }
+
+    public override async void PlayerSuperAttack()
+    {
+        //base.PlayerSuperAttack();
+        GameObject gob = Instantiate(_bullet, _muzzle);
+        await Task.Delay(_superAttackDelay);
+        _bulletStop = false;
+    }
+}
---
+++ Assets/2Kanbara/Scripts Kanbara/TestBulletController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBulletController : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    [SerializeField] Vector2 _bulletspeed = default;
+    float _time;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.AddForce(_bulletspeed, ForceMode2D.Impulse);
+    }
+}
