5f112535e86200941010e4d18c35bd4fb52b5214
安達青 <sei.dance.dream@gmail.com>
2021-12-07 20:31:09 +0900

    プレイヤーのバレット制作中

--- Assets/5Adachi/Scripts/PlayerBullet.cs
+++ Assets/5Adachi/Scripts/PlayerBullet.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerBullet : BulletBese
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] string _enemyTag;
+    [SerializeField] float _bulletSpeed = 10f;
+    [SerializeField] Vector2 _direction = Vector2.up;
+    Rigidbody2D _rb;
     public override void BulletMovement()
     {
-        base.BulletMovement();
+        
     }
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        Vector2 v = _direction.normalized * _bulletSpeed;
+        _rb.velocity = v;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == _enemyTag)
+        {
+            var enemy = GameObject.FindGameObjectsWithTag(_enemyTag);
+
+            foreach(var e in enemy)
+            {
+                Destroy(e);
+            }
+        }
+    } 
+        
 }
