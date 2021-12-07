d003222514259377efd67bf791214f14877f7f23
近藤　倫太 <tora_goraias@gmail.com>
2021-12-07 20:25:53 +0900

    ボムの処理を製作中

--- Assets/6Kondo/Scripts/PlayerBomb.cs
+++ Assets/6Kondo/Scripts/PlayerBomb.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class PlayerBomb : BulletBese
 {
+    [SerializeField] string _enemyTag;
+    [SerializeField] string _enemyBulletTag;
+    [SerializeField] Vector2 _direction = Vector2.up;
+    [SerializeField] float _speed = 10f;
+    Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        Vector3 v = _direction.normalized * _speed;
+        _rb.velocity = v;
+    }
     public override void BulletMovement()
     {
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == _enemyTag)
+        {
+            var enemy = GameObject.FindGameObjectsWithTag(_enemyTag);
+            var bullet = GameObject.FindGameObjectsWithTag(_enemyBulletTag);
 
+            foreach(var e in enemy)
+            {
+                Destroy(e);
+            }
+
+            foreach(var b in bullet)
+            {
+                Destroy(b);
+            }
+        }
+    }
 }
