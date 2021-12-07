b211818155ce5d2c7af1444453cf2ba8ff408cb5
和田　有矢 <88494642+BAKUSOUMARU@users.noreply.github.com>
2021-12-07 20:32:17 +0900

    Enemyの基底クラスを作成中

---
+++ Assets/3Wada/Scripts Wada/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : EnemyBese
+
+{
+    [SerializeField] Transform _muzzle;
+    [SerializeField] GameObject _bullet;
+    [SerializeField] float m_fireInterval = 1f;
+
+    public override void Attack()
+    {
+        // 各 muzzle から弾を発射する
+        Instantiate(_bullet,_muzzle);
+    }
+}
