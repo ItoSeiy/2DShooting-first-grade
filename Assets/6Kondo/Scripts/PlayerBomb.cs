889520f261950d09352231ec0467d77ca641876d
近藤　倫太 <tora_goraias@gmail.com>
2021-12-07 19:27:23 +0900

    ボムの処理を製作中

---
+++ Assets/6Kondo/Scripts/PlayerBomb.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBomb : BulletBese
+{
+    public override void BulletMovement()
+    {
+
+    }
+
+
+}
