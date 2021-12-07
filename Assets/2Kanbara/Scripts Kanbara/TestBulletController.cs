31601a3a1503dec1f61472936b92264e37057b4e
神原琉成 <tekunikaru22@gmail.com>
2021-12-07 20:32:49 +0900

    コードを短くした（テスト用のバレットコントローラー）

--- Assets/2Kanbara/Scripts Kanbara/TestBulletController.cs
+++ Assets/2Kanbara/Scripts Kanbara/TestBulletController.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TestBulletController : MonoBehaviour
 {
     Rigidbody2D _rb;
     [SerializeField] Vector2 _bulletspeed = default;
-    float _time;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
