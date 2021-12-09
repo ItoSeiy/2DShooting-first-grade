151c3051da37febff045720da5890749698b9780
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-09 18:02:24 +0900

    EnemyBaseを修正中

--- Assets/1Ito/Scripts Ito/EnemyBese.cs
+++ Assets/1Ito/Scripts Ito/EnemyBese.cs
@@ -10,14 +10,14 @@ using UnityEngine;
 public abstract class EnemyBese : MonoBehaviour, IDamage
 {
     public float EnemyHp { get => _enemyHp;}
-    public float AddDamageRatio { get => _addDamageRatio;}
+    public float AddDamageRatio { get => _damageRatio;}
 
     [SerializeField, Header("体力")] private float _enemyHp = default;
     [SerializeField, Header("攻撃頻度(秒)")] private float _attackInterval = default;
     /// <summary>
     /// 攻撃力の割合
     /// </summary>
-    [SerializeField, Header("攻撃力を何割にするか"), Range(0f, 1f)] float _addDamageRatio = default;
+    [SerializeField, Header("攻撃力を何割にするか"), Range(0f, 1f)] float _damageRatio = default;
     private float _timer = default;
 
     private void Update()
@@ -63,7 +63,7 @@ public abstract class EnemyBese : MonoBehaviour, IDamage
     void IDamage.AddDamage(float damage)
     {
         //攻撃力を設定した分減らす処理
-        damage /= _addDamageRatio;
+        damage /= _damageRatio;
 
         _enemyHp -= damage;
         OnGetDamage();
