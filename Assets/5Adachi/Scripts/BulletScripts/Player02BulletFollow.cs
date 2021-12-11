11cf19bc3dce42af28d051f0145d6d41a261f3b8
安達青 <sei.dance.dream@gmail.com>
2021-12-11 12:53:30 +0900

    プレイヤーのバレット制作中

---
+++ Assets/5Adachi/Scripts/BulletScripts/Player02BulletFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player02BulletFollow : BulletBese
+{
+    [SerializeField] float m_speed = 1f;
+    Vector2 _direction;
+    GameObject _enemy;
+    Rigidbody2D _rb;
+    float _timer;
+    [SerializeField, Header("追従する時間")] float _followTime = 2f;
+
+    private void Start()
+    {
+        _enemy = GameObject.FindGameObjectWithTag("enemy");
+        if(_enemy)
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _followTime) return;
+        _direction = _enemy.transform.position - this.transform.position;
+        _direction = _direction.normalized * m_speed;
+        _rb.velocity = _direction;
+    }
+}
