94ceef2c5e41124b1f6196e5e41e3f35606c57ff
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-11 13:03:16 +0900

    Bulletの修正を完了した

--- Assets/1Ito/Scripts Ito/Bullet/BulletBese.cs
+++ Assets/1Ito/Scripts Ito/Bullet/BulletBese.cs
@@ -12,18 +12,32 @@ public abstract class BulletBese : MonoBehaviour
     public float Damage { get => _damage;}
     
     [SerializeField, Header("Bulletが与えるダメージ")] private float _damage;
+    [SerializeField, Header("Bulletの動く向き")] Vector2 _direction = Vector2.up;
+    [SerializeField, Header("")] float _speed = default;
+    private void Start()
+    {
+        BulletMove();
+    }
 
-    
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         BulletAttack(collision);
     }
 
     /// <summary>
-    /// Bulletの基底クラスの関数
+    /// Bulletがまっすぐ飛ぶ記述がデフォルトではされている
+    /// 変則的な動きを行いたければオーバライドをすること
+    /// </summary>
+    protected virtual void BulletMove()
+    {
+        var _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = _direction.normalized * _speed;
+    }
+
+    /// <summary>
+    /// 基本的にはオーバライドを行わなくてもよい
     /// 衝突した相手のインタフェースを参照し攻撃を加える関数
-    /// 派生クラスではbase.BulletAttack(col);と記述する(基底クラスの機能を呼び出せる)
+    /// オーバライドする際は中身に[base.BulletAttack(col);]と記述する(基底クラスの機能を呼び出せる)
     /// </summary>
     /// <param name="col">当たった相手のコライダー</param>
     protected virtual void BulletAttack(Collider2D col)
--- Assets/1Ito/Scripts Ito/Bullet/TestBullet01.cs
+++ Assets/1Ito/Scripts Ito/Bullet/TestBullet01.cs
@@ -4,9 +4,4 @@ using UnityEngine;
 
 public class TestBullet01 : BulletBese
 {
-    protected override void BulletAttack(Collider2D col)
-    {
-
-        base.BulletAttack(col);
-    }
 }
