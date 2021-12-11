b5f7abe0257b12a46b9ce9d85dd5540dcb212213
和田　有矢 <88494642+BAKUSOUMARU@users.noreply.github.com>
2021-12-11 18:24:02 +0900

    モブ敵の作成中

---
+++ Assets/3Wada/Scripts Wada/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : EnemyBese
+{
+    [SerializeField] GameObject[] _muzzle;
+
+    protected override void Attack()
+    {
+      
+    }
+
+    protected override void OnGetDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
