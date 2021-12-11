d6c12c5971e63bc2af31763f74173fa04ad8eb65
和田　有矢 <88494642+BAKUSOUMARU@users.noreply.github.com>
2021-12-11 20:06:27 +0900

    モブ敵制作中

--- Assets/3Wada/Scripts Wada/EnemyController.cs
+++ Assets/3Wada/Scripts Wada/EnemyController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class EnemyController : EnemyBese
 {
-    [SerializeField] GameObject[] _muzzle;
+    [SerializeField] Transform[] _muzzle = null;
+    [SerializeField] GameObject _bullet = null;
+    [SerializeField] AudioSource _onDestroyAudio = null;
 
     protected override void Attack()
     {
-      
+        for (int i = 0;i < _muzzle.Length; i++)
+        {
+            Instantiate(_bullet, _muzzle[i]);
+        }
     }
 
     protected override void OnGetDamage()
     {
-        throw new System.NotImplementedException();
-    }
-
-    // Start is called before the first frame update
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        if (EnemyHp == 0) 
+        {
+            AudioSource.PlayClipAtPoint(_onDestroyAudio.clip,transform.position);
+            
+        }
+    }    
 }
