a1fde04d67efc5bee09e37b7fb2739d1609980d0
安達青 <sei.dance.dream@gmail.com>
2022-01-09 11:24:24 +0900

    EnemyBaseをBossEnemyに継承

---
+++ Assets/5Adachi/Scripts/BossEnemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : EnemyBese
+{
+    protected override void OnGetDamage()
+    {
+
+    }
+    protected override void Attack()
+    {
+
+    }
+}
