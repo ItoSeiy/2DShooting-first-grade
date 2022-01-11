d714fb8223ae6486507b3e0d36c727a622a2b4bf
伊東聖矢 <tvedaamamesab@gmail.com>
2022-01-11 21:06:26 +0900

    オブジェクトプールに適用するようにBulletに変更を加えた

--- Assets/5Adachi/Scripts/BulletScripts/BulletParent.cs
+++ Assets/5Adachi/Scripts/BulletScripts/BulletParent.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class BulletParent : MonoBehaviour
 {
-    BulletBese[] _bulletChildren = null;
-    Vector2[] _initialBulletChildrenPos = default;
+    GameObject[] _bulletsChildren = new GameObject[99];
+    Vector2[] _initialBulletChildrenPos = new Vector2[99];
     private void Awake()
     {
-        _bulletChildren = GetComponentsInChildren<BulletBese>();
-        for(int i = 0; i < _bulletChildren.Length; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            _initialBulletChildrenPos[i] = _bulletChildren[i].transform.position;
+            _bulletsChildren[i] = transform.GetChild(i).gameObject;
+            _initialBulletChildrenPos[i] = _bulletsChildren[i].transform.position;
         }
     }
+
     void OnDisable()
     {
-        for(int i = 0; i < _bulletChildren.Length; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            _bulletChildren[i].transform.position = _initialBulletChildrenPos[i];
+            _bulletsChildren[i].transform.position = _initialBulletChildrenPos[i];
         }
     }
 }
