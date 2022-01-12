2d7e0ee330f9b5d3c8f3fd80fe841320fd826032
和田　有矢 <88494642+BAKUSOUMARU@users.noreply.github.com>
2022-01-12 19:47:02 +0900

    モブ敵の派生クラスの修正

--- Assets/3Wada/Scripts Wada/EnemyController.cs
+++ Assets/3Wada/Scripts Wada/EnemyController.cs
@@ -7,106 +7,80 @@ using UnityEngine;
 public class EnemyController : EnemyBese
 {
     [SerializeField,Header("マズルの位置")] Transform[] _muzzle = null;
-    [SerializeField, Header("バレット")] GameObject _bullet = null;
     [SerializeField, Header("倒された時の音")] AudioSource _onDestroyAudio = null;
     [SerializeField, Header("移動の向きの変えるY軸")] float _ybottomposition = 0;
     [SerializeField, Header("移動の向きの変えるx軸")] float _xbottomposition = 0;
     [SerializeField, Header("出た時の移動方向")] Vector2 _beforeDir;
     [SerializeField, Header("移動変わった後の移動方向")] Vector2 _afterDir;
-    [SerializeField,Header("モブ敵を止める時の右.左.上.下の切り替え")] MoveMode _moveMode;
-    Rigidbody2D _rb = null;
+    [SerializeField,Header("モブ敵を止める時の方向の切り替え")] MoveMode _moveMode;
     bool _isBttomposition = false;
     [SerializeField, Header("何秒とどまるか")] float _stopcount = 0.0f;
-    private float _timer1 = default;
-    [SerializeField, Header("攻撃頻度(秒)")] private float _attackInterval1 = 1f;
 
-    /// <summary>
-    /// 出た時の移動
-    /// </summary>
-    void Start()
+
+    private void OnEnable()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity = _beforeDir;
+        Rb.velocity = _beforeDir;
     }
 
-
-     void Update()
-    {   
+     protected override void Update()
+     {
+        base.Update();
         if (_isBttomposition) return;
-        
-        if(_moveMode == MoveMode.right)
-        {
-            if( this.transform.position.x <= _xbottomposition)
-            {
-                EnemyMove();
-            }   
-        }
-        if(_moveMode == MoveMode.left)
-        {
-            if(this.transform.position.x >= _xbottomposition)
-            {
-                EnemyMove();
-            }
-        }
-        if(_moveMode == MoveMode.up)
-        {
-            if (this.transform.position.y <= _ybottomposition)
-            {
-                EnemyMove();
-            }
-        }
-        if(_moveMode == MoveMode.down)
-        {
-            if(this.transform.position.y >= _ybottomposition)
-            {
-                EnemyMove();
-            }
-        }
-        _timer1 += Time.deltaTime;
 
-        if (_timer1 > _attackInterval1)
+        switch (_moveMode)
         {
-            Danmaku();
-            _timer1 = 0;
+            case MoveMode.right:
+                if (this.transform.position.x <= _xbottomposition)
+                {
+                    EnemyMove();
+                }
+                break;
+            case MoveMode.left:
+                if (this.transform.position.x >= _xbottomposition)
+                {
+                    EnemyMove();
+                }
+                break;
+            case MoveMode.up:
+                if (this.transform.position.y <= _ybottomposition)
+                {
+                    EnemyMove();
+                }
+                break;
+            case MoveMode.down:
+                if (this.transform.position.y >= _ybottomposition)
+                {
+                    EnemyMove();
+                }
+                break;
         }
-    }
+     }
 
 
     void EnemyMove()
     {   
-        /// <summary>
-        /// 途中で止まる時の処理
-        /// </summary>
-        _rb.velocity = Vector2.zero;
+        //途中で止まる時の処理
+        Rb.velocity = Vector2.zero;
         _stopcount -= Time.deltaTime;
         /// <summary>
         /// また動き出す時の処理
         /// </summary>
         if (_stopcount <= 0)
         {
-            _rb.velocity = _afterDir;
-            _isBttomposition = true;
-            
+            Rb.velocity = _afterDir;
+            _isBttomposition = true; 
         }
     }
 
-    public void Danmaku()
+   
+    protected override void Attack()
     {
         for (int i = 0; i < _muzzle.Length; i++)
         {
-            Instantiate(_bullet, _muzzle[i]);
+            //Instantiate(_bullet, _muzzle[i]);
         }
     }
-    /// <summary>
-    /// 弾幕を出す処理
-    /// </summary>
-    protected override void Attack()
-    {      
-    }
 
-    /// <summary>
-    /// 倒れた時に流す音の処理
-    /// </summary>
     protected override void OnGetDamage()
     {
         if (EnemyHp == 0) 
