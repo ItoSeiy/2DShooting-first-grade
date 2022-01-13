1dfd03f2566d1349fa7b009b26ef04f8c3b7e345
近藤　倫太 <tora_goraias@gmail.com>
2022-01-13 18:28:45 +0900

    ボムの機能を製作中

--- Assets/6Kondo/Scripts/Player01Bomb.cs
+++ Assets/6Kondo/Scripts/Player01Bomb.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class Player01Bomb : BulletBese
 {
-    [SerializeField] string _enemyTag;
+    //[SerializeField] string _enemyTag;
     [SerializeField] string _enemyBulletTag;
-    [SerializeField] Vector2 _direction = Vector2.up;
-    [SerializeField] float _speed = 10f;
+    //[SerializeField] Vector2 _direction = Vector2.up;
+    
     [SerializeField] GameObject _bombChildBullet = null;
     [SerializeField] Transform[] _muzzle = null;
-    Rigidbody2D _rb;
+    //Rigidbody2D _rb;
     public Explosion m_explosionPrefab;
 
-    void OnBecameInvisible()
+    //void OnBecameInvisible()
+    //{
+    //    Destroy(this.gameObject);
+    //}
+
+    protected override void BulletMove()
     {
-        Destroy(this.gameObject);
+        base.BulletMove();
     }
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity = _direction.normalized * _speed;
-    }
+    //private void Start()
+    //{
+    //    _rb = GetComponent<Rigidbody2D>();_speed;
+    //}
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == _enemyTag)
+        if (collision.tag == EnemyTag)
         {
             Instantiate(m_explosionPrefab, collision.transform);
             for (int i = 0; i < _muzzle.Length; i++)
