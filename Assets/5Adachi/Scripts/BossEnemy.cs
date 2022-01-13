0ccbbf0b5d611bae14c7ddb0d3c0735c8da6447a
安達青 <sei.dance.dream@gmail.com>
2022-01-13 16:52:04 +0900

    ボス敵制作中

--- Assets/5Adachi/Scripts/BossEnemy.cs
+++ Assets/5Adachi/Scripts/BossEnemy.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BossEnemy : EnemyBese
 {
+    [SerializeField] string _bombTag = null;
     protected override void Update()
     {
         base.Update();
-
+        //if()
     }
     protected override void OnGetDamage()
     {
