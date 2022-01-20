bf16251f96e9700d1cadd54973c3a0fdec99767d
近藤　倫太 <tora_goraias@gmail.com>
2022-01-20 20:36:06 +0900

    ボムのエフェクトを製作中

--- Assets/6Kondo/Scripts/Player01Bomb.cs
+++ Assets/6Kondo/Scripts/Player01Bomb.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
 using System.Linq;
+using DG.Tweening;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Player01Bomb : BulletBese
 {
     [SerializeField] Explosion _explosionPrefab　= null;
     [SerializeField] float _childBulletDelay = 1f;
+    [SerializeField] Light2D _grobalLight = null;
+    [SerializeField] float _goalColor = default;
+    [SerializeField] float _changeValueInterval = default;
     [SerializeField] Transform[] _muzzle = null;
     SpriteRenderer _sprite = null;
+    [SerializeField] float _lightChangeDelay = default;
 
+    Color _test = default;
+    Color _test2 = default;
     protected override void Awake()
     {
         base.Awake();
@@ -30,6 +38,28 @@ public class Player01Bomb : BulletBese
             var explosionPrefab = Instantiate(_explosionPrefab);
             explosionPrefab.transform.position = collision.transform.position;
 
+            var sequence = DOTween.Sequence(DOTween.To(() => _grobalLight.color.g,
+                (x) =>
+                {
+                    Color c = _grobalLight.color;
+                    _test = _grobalLight.color;
+                    c.g = x;
+                    _grobalLight.color = c;
+                },
+                _goalColor,
+                _changeValueInterval));
+            sequence.Append(DOTween.To(() => _grobalLight.color.b,
+                (x) =>
+                {
+                    Color c = _grobalLight.color;
+                    _test2 = _grobalLight.color;
+                    c.b = x;
+                    _grobalLight.color = c;
+                },
+                _goalColor,
+                _changeValueInterval));
+            RetrunColor();
+
             _sprite.enabled = false;
             Rb.velocity = Vector2.zero;
             for (int i = 0; i < _muzzle.Length; i++)
@@ -48,5 +78,27 @@ public class Player01Bomb : BulletBese
         }
     }
 
+    public async void RetrunColor()
+    {
+        await Task.Delay(3);
+        var sequence = DOTween.Sequence(DOTween.To(() => _grobalLight.color.g,
+                (x) =>
+                {
+                    Color c = _grobalLight.color;
+                    c.g = x;
+                    _grobalLight.color = c;
+                },
+                _test.g,
+                _changeValueInterval));
+        sequence.Append(DOTween.To(() => this._grobalLight.color.b,
+            (x) =>
+            {
+                Color c = _grobalLight.color;
+                c.b = x;
+                _grobalLight.color = c;
+            },
+            _test2.b,
+            _changeValueInterval));
 
+    }
 }
