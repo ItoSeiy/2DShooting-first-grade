5ab9a55079281ba2b55bd006274b7ca00fbe9d12
安達青 <sei.dance.dream@gmail.com>
2022-01-21 18:57:07 +0900

    ボス敵制作中

--- Assets/5Adachi/Scripts/BossEnemyMove03.cs
+++ Assets/5Adachi/Scripts/BossEnemyMove03.cs
@@ -6,27 +6,29 @@ public class BossEnemyMove03 : MonoBehaviour
 {
     Rigidbody2D _rb;
 
-    public bool _isMove = true;
+    public bool _isMove = false;
     int _count = 0;
     public float x;
     private GameObject _player;
     [SerializeField] private string _playerTag = null;
 
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _player = GameObject.FindGameObjectWithTag(_playerTag);
-        StartCoroutine("Vertical");
+        
     }
 
     private void Update()
     {
-
+        StartCoroutine("Vertical");
     }
 
     IEnumerator Vertical()
     {
-            transform.position = new Vector2(Mathf.Clamp(transform.position.x, -8f, 8f), Mathf.Clamp(transform.position.y, -4f, 4f));
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -8f, 8f), Mathf.Clamp(transform.position.y, -4f, 4f));
+        _isMove = true;
             if (_isMove)
             {
                 x = _player.transform.position.x;
@@ -47,6 +49,7 @@ public class BossEnemyMove03 : MonoBehaviour
                         //yield return new WaitForSeconds();
                     }
                 }
+            
 
                 if (_count == 1)
                 {
@@ -68,7 +71,7 @@ public class BossEnemyMove03 : MonoBehaviour
                         StartCoroutine(Down());
                     }
                 }
-            }
+        }
 
             IEnumerator Down()
             {
@@ -104,4 +107,5 @@ public class BossEnemyMove03 : MonoBehaviour
                 }
             }
         }
+    
 }
