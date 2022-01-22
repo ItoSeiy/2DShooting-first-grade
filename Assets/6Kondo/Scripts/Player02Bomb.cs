d4a9a725f3ad971cadeddf6e8abae448195a5a56
近藤　倫太 <tora_goraias@gmail.com>
2022-01-22 18:51:07 +0900

    Player02Bombの機能を製作中

--- Assets/6Kondo/Scripts/Player02Bomb.cs
+++ Assets/6Kondo/Scripts/Player02Bomb.cs
@@ -6,32 +6,21 @@ using System.Linq;
 
 public class Player02Bomb : BulletBese
 {
-    [SerializeField] Explosion _explosionPrefab = null;
     [SerializeField] float _childBulletDelay = 1f;
     [SerializeField] Transform[] _muzzle = null;
-    SpriteRenderer _sprite = null;
-    protected override void OnTriggerEnter2D(Collider2D collision)
+    protected override void OnEnable()
     {
-        base.BulletAttack(collision);
-        StartCoroutine(UseBombChildBullet(collision));
+        StartCoroutine(UseBombChildBullet());
     }
 
-    IEnumerator UseBombChildBullet(Collider2D collision)
+    IEnumerator UseBombChildBullet()
     {
-        if (collision.tag == OpponenTag)
+        for (int i = 0; i < _muzzle.Length; i++)
         {
-            Rb.velocity = Vector2.zero;
-            for (int i = 0; i < _muzzle.Length; i++)
-            {
-                var bombChild = PlayerBulletPool.Instance.UseBullet(_muzzle[i].position, PlayerBulletType.BombChild);
-                bombChild.transform.rotation = _muzzle[i].rotation;
-                yield return new WaitForSeconds(_childBulletDelay);
-            }
-            gameObject.SetActive(false);
+            var bombChild = PlayerBulletPool.Instance.UseBullet(_muzzle[i].position, PlayerBulletType.BombChild);
+            bombChild.transform.rotation = _muzzle[i].rotation;
+            yield return new WaitForSeconds(_childBulletDelay);
         }
-        else if (collision.tag == GameZoneTag)
-        {
-            gameObject.SetActive(false);
-        }
+        gameObject.SetActive(false);
     }
 }
