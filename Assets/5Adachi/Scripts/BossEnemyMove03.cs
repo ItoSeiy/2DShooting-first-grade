da13b2d055a0f9f89ced75216142ee1428ca06da
安達青 <sei.dance.dream@gmail.com>
2022-01-22 16:07:29 +0900

    ボス敵制作中

--- Assets/5Adachi/Scripts/BossEnemyMove03.cs
+++ Assets/5Adachi/Scripts/BossEnemyMove03.cs
@@ -8,6 +8,7 @@ public class BossEnemyMove03 : MonoBehaviour
 
     public bool _isMove = false;
     int _count = 0;
+    int _vectorCount = 0;
     public float x;
     private GameObject _player;
     [SerializeField] private string _playerTag = null;
@@ -18,93 +19,108 @@ public class BossEnemyMove03 : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _player = GameObject.FindGameObjectWithTag(_playerTag);
         _isMove = true;
+        StartCoroutine(Vertical());
     }
 
     private void Update()
     {
-        StartCoroutine(Vertical());
+
     }
 
     IEnumerator Vertical()
     {
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, -8f, 8f), Mathf.Clamp(transform.position.y, -4f, 4f));
-        if (_isMove)
+        while (_isMove)
         {
-            x = _player.transform.position.x;
-            if (_count == 0)
-            {
-                if (_player.transform.position.x >= transform.position.x)
-                {
-                    Debug.Log("a");
-                    _count = 1;
-                    _rb.velocity = new Vector2(x, 0);
-                    //yield return new WaitForSeconds();
-                }
-                else// if (_player.transform.position.x <= transform.position.x)
-                {
-                    Debug.Log("i");
-                    _count = 2;
-                    _rb.velocity = new Vector2(x, 0);
-                    //yield return new WaitForSeconds();
-                }
-            }
-
-
-            if (_count == 1)
-            {
-                if (_player.transform.position.x <= transform.position.x)
-                {
-                    _rb.velocity = new Vector2(0, 0);
-                    yield return new WaitForSeconds(0.5f);
-                    StartCoroutine(Down());
-                }
-            }
-
-            if (_count == 2)
-            {
-                if (_player.transform.position.x >= transform.position.x)
-                {
-                    Debug.Log(_isMove);
-                    _rb.velocity = new Vector2(0, 0);
-                    yield return new WaitForSeconds(0.5f);
-                    StartCoroutine(Down());
-                }
-            }
-        }
-
-            IEnumerator Down()
+            if (_vectorCount == 0)
             {
                 if (_isMove)
                 {
-                    float y = _player.transform.position.y;
-                    _rb.velocity = new Vector2(0, y * 3);
-                    yield return new WaitForSeconds(0.5f);
-
-                    if (_player.transform.position.y >= transform.position.y)
+                    x = _player.transform.position.x;
+                    if (_count == 0)
                     {
-                        _isMove = false;
-                        Debug.Log(_isMove);
-                        _rb.velocity = new Vector2(0, 0);
-                        yield return new WaitForSeconds(1);
-                        StartCoroutine(Up());
+                        if (_player.transform.position.x >= transform.position.x)
+                        {
+                            Debug.Log("a");
+                            _count = 1;
+                            _rb.velocity = new Vector2(x, 0);
+                            //yield return new WaitForSeconds();
+                        }
+                        else// if (_player.transform.position.x <= transform.position.x)
+                        {
+                            Debug.Log("i");
+                            _count = 2;
+                            _rb.velocity = new Vector2(x, 0);
+                            //yield return new WaitForSeconds();
+                        }
                     }
-                    else if (transform.position.y <= -4)
+
+
+                    if (_count == 1)
                     {
-                        _isMove = false;
-                        _rb.velocity = new Vector2(0, 0);
-                        yield return new WaitForSeconds(1);
-                        StartCoroutine(Up());
+                        if (_player.transform.position.x <= transform.position.x)
+                        {
+                            _rb.velocity = new Vector2(0, 0);
+                            yield return new WaitForSeconds(0.5f);
+                            //StartCoroutine(Down());
+                            _vectorCount = 1;
+                        }
+                    }
+
+                    if (_count == 2)
+                    {
+                        if (_player.transform.position.x >= transform.position.x)
+                        {
+                            Debug.Log(_isMove);
+                            _rb.velocity = new Vector2(0, 0);
+                            yield return new WaitForSeconds(0.5f);
+                            //StartCoroutine(Down());
+                            _vectorCount = 1;
+                        }
                     }
                 }
 
-                IEnumerator Up()
+                //IEnumerator Down()
+                if (_vectorCount == 1)
                 {
-                    _rb.velocity = new Vector2(0, 3);
-                    yield return new WaitForSeconds(3);
-                    _rb.velocity = new Vector2(0, 0);
-                    //yield break;
+                    if (_isMove)
+                    {
+                        float y = _player.transform.position.y;
+                        _rb.velocity = new Vector2(0, y * 3);
+                        yield return new WaitForSeconds(0.5f);
+
+                        if (_player.transform.position.y >= transform.position.y)
+                        {
+                            //_isMove = false;
+                            Debug.Log(_isMove);
+                            _rb.velocity = new Vector2(0, 0);
+                            yield return new WaitForSeconds(1);
+                            //StartCoroutine(Up());
+                            _vectorCount = 2;
+                        }
+                        else if (transform.position.y <= -4)
+                        {
+                            //_isMove = false;
+                            _rb.velocity = new Vector2(0, 0);
+                            yield return new WaitForSeconds(1);
+                            //StartCoroutine(Up());
+                            _vectorCount = 2;
+                        }
+                    }
+
+                    //IEnumerator Up()
+                    if (_vectorCount == 2)
+                    {
+                        _rb.velocity = new Vector2(0, 3);
+                        yield return new WaitForSeconds(3);
+                        _rb.velocity = new Vector2(0, 0);
+                        yield return new WaitForSeconds(2);
+                        _vectorCount = 0;
+                        StartCoroutine(Vertical());
+                        //yield break;
+                    }
                 }
             }
         }
-    
+    }
 }
