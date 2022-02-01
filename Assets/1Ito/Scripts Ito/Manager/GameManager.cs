f56bf43746f6f48bb6174683445ec89b35c79001
神原琉成 <tekunikaru22@gmail.com>
2022-02-01 17:13:28 +0900

    攻撃にインターバルを作った

--- Assets/1Ito/Scripts Ito/Manager/GameManager.cs
+++ Assets/1Ito/Scripts Ito/Manager/GameManager.cs
@@ -9,6 +9,8 @@ using UnityEngine.Events;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    public string MainCharacterName => _mainCharacterName;
+    [SerializeField, Header("メインキャラクターの名前")] string _mainCharacterName = default;
     public static GameManager Instance => _instance;
     static GameManager _instance;
 
