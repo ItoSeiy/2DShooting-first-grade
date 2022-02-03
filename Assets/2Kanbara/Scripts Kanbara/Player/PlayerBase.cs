52b34be66d9c742c76db704f05aebbb96c3a1d4a
神原琉成 <tekunikaru22@gmail.com>
2022-02-03 13:43:41 +0900

    エラー修正

--- Assets/2Kanbara/Scripts Kanbara/Player/PlayerBase.cs
+++ Assets/2Kanbara/Scripts Kanbara/Player/PlayerBase.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Threading.Tasks;
 using UnityEngine.InputSystem;
 using Cinemachine;
+using DG.Tweening;
 
 
 /// <summary>
@@ -16,6 +17,7 @@ public class PlayerBase : MonoBehaviour
 {
     [SerializeField, Header("プレイヤーの音")] protected AudioData[] _audioData;
     Rigidbody2D _rb;
+    SpriteRenderer _sp;
     protected AudioSource _audioSource;
     Animation _anim;
     Vector2 _dir;
@@ -23,31 +25,30 @@ public class PlayerBase : MonoBehaviour
     [SerializeField, Header("リスポーンするポジション")] public Transform _playerRespawn = default;
     [SerializeField, Header("弾を発射するポジション")] public Transform _muzzle = default;
 
-    [SerializeField, Header("精密操作時の発射する間隔(ミリ秒)")] public int _superAttackDelay = default;
-    [SerializeField, Header("発射する間隔(ミリ秒)")] public int _attackDelay = default;
-    [SerializeField, Header("チャージショットの発射する間隔（ミリ秒）")] int _chargeAttackDelay = default;
+    [SerializeField, Header("精密操作時の発射する間隔(ミリ秒)")] public int _superAttackDelay = 200;
+    [SerializeField, Header("発射する間隔(ミリ秒)")] public int _attackDelay = 200;
     [SerializeField, Header("ボムのクールタイム（ミリ秒）")] public int _bombCoolTime = default;
-    [SerializeField, Header("無敵モードのクールタイム")] public int _invincibleCoolTime = default;
+    [SerializeField, Header("無敵モードのクールタイム")] public int _invincibleCoolTime = 2800;
 
     [SerializeField, Header("この数値未満ならレベル１")] int _playerLevel1Denom = default;
     [SerializeField, Header("レベル１以上のとき、この数値未満ならレベル２")] int _playerLevel2Denom = default;
     [SerializeField, Header("この数値までがレベル３")] int _playerLevel3Denom = default;
 
-    [SerializeField, Header("Enemyのタグ")] string _enemyTag = default;
-    [SerializeField, Header("EnemyのBulletのタグ")] string _enemyBulletTag = default;
-    [SerializeField, Header("Powerのタグ")] string _powerTag = default;
-    [SerializeField, Header("Pointのタグ")] string _pointTag = default;
-    [SerializeField, Header("1upのタグ")] string _1upTag = default;
-    [SerializeField, Header("ボムを増やすアイテムのタグ")] string _bombItemTag = default;
-    [SerializeField, Header("Invincibleのタグ")] string _invincibleTag = default;
+    [SerializeField, Header("Enemyのタグ")] string _enemyTag = "Enemy";
+    [SerializeField, Header("EnemyのBulletのタグ")] string _enemyBulletTag = "EnemyBullet";
+    [SerializeField, Header("Powerのタグ")] string _powerTag = "Power";
+    [SerializeField, Header("Pointのタグ")] string _pointTag = "Point";
+    [SerializeField, Header("1upのタグ")] string _1upTag = "1UP";
+    [SerializeField, Header("ボムを増やすアイテムのタグ")] string _bombItemTag = "BombItem";
+    [SerializeField, Header("Invincibleのタグ")] string _invincibleTag = "Invincible";
 
     [SerializeField, Header("動くスピード")] float _moveSpeed = default;
     [SerializeField, Header("精密操作のスピード")] float _lateMove = default;
 
     [SerializeField, Header("この数値以上なら、一定時間無敵モードになる変数")] int _invincibleMax = default;
     [SerializeField, Header("Playerのパワーの上限")] int _playerPowerMax = default;
-    [SerializeField, Header("リスポーン中の無敵時間")] protected int _respawnTime = default;
-    [SerializeField, Header("リスポーン後の無敵時間")] int _afterRespawnTime = default;
+    [SerializeField, Header("リスポーン中の無敵時間")] protected int _respawnTime = 2800;
+    [SerializeField, Header("リスポーン後の無敵時間")] int _afterRespawnTime = 1400;
 
     [SerializeField, Header("死亡時のアニメーション")] Animation _dead = default;
 
@@ -55,12 +56,15 @@ public class PlayerBase : MonoBehaviour
 
     [SerializeField, Header("揺らすカメラ")] CinemachineVirtualCamera _cmvcam1 = default;
 
-    [SerializeField, Header("通常弾の音")] protected string _playerBulletAudio = default;
-    [SerializeField, Header("精密操作時の弾の音")] protected string _playerSuperBulletAudio = default;
-    [SerializeField, Header("チャージ中の音")] protected string _playerChargeBulletAudio = default;
-    [SerializeField, Header("チャージショットの音")] protected string _playerChargeShotBulletAudio = default;
-    [SerializeField, Header("ボムの音")] protected string _playerBombShotAudio = default;
-    [SerializeField, Header("プレイヤーの被弾時に流れる音")] protected string _playerDestroyAudio = default;
+    [SerializeField, Header("通常弾の音")] protected string _playerBulletAudio = "Bullet";
+    [SerializeField, Header("精密操作時の弾の音")] protected string _playerSuperBulletAudio = "SuperBullet";
+    [SerializeField, Header("チャージ中の音")] protected string _playerChargeBulletAudio = "Charge";
+    [SerializeField, Header("チャージショットの音")] protected string _playerChargeShotBulletAudio = "ChargeShot";
+    [SerializeField, Header("ボムの音")] protected string _playerBombShotAudio = "Bomb";
+    [SerializeField, Header("プレイヤーの被弾時に流れる音")] protected string _playerDestroyAudio = "PlayerDestroy";
+
+    [SerializeField] Color _color = default;
+    [SerializeField] float _changeValueInterval = default;
 
     protected const int _level1 = 1;
     protected const int _level2 = 2;
@@ -109,6 +113,7 @@ public class PlayerBase : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _sp = GetComponent<SpriteRenderer>();
         _audioSource = GetComponent<AudioSource>();
         _anim = GetComponent<Animation>();
         _audioData = GetComponent<AudioData[]>();
@@ -157,6 +162,14 @@ public class PlayerBase : MonoBehaviour
                 }
                 break;
         }
+        if (_dir.x > _default)
+        {
+            _sp.flipX = false;
+        }
+        else if (_dir.x < _default)
+        {
+            _sp.flipX = true;
+        }
     }
     public void OnMove(InputAction.CallbackContext context)//通常の移動
     {
@@ -180,28 +193,28 @@ public class PlayerBase : MonoBehaviour
         }
     }
 
-    public void OnJump(InputAction.CallbackContext context)//SpaceKeyが押された瞬間の処理
+    public async void OnJump(InputAction.CallbackContext context)//SpaceKeyが押された瞬間の処理
     {
-        if (_isNotControll) return;
-        if (BombCount >= _default)
+        if(context.started && BombCount > _default && !_isBomb && !_isNotControll)
         {
             Bom();
-            _isBomb = true;
+            await Task.Delay(_bombCoolTime);
+            _isBomb = false;
             GameManager.Instance.PlayerBombCountChange(_defaultDown);
+            _bombCount = GameManager.Instance.PlayerBombCount;
         }
     }
 
     public void OnInputChargeShotButton(InputAction.CallbackContext context)//ChargeShotの処理
     {
-        if (_isAttackMode) return;
-        if (context.started)
+        if (context.started && !_wasCharge && !_isAttackMode)
         {
             _cmvcam1.Priority = 10;
             _audioSource.Stop();
             Play(_playerChargeBulletAudio);
             _wasCharge = true;
         }
-        if(context.performed)
+        if(context.performed && _wasCharge)
         {
             PlayerChargeAttack();
             _wasCharge = false;
@@ -241,19 +254,17 @@ public class PlayerBase : MonoBehaviour
     }
 
     /// <summary>チャージショット時の攻撃処理</summary>
-    public virtual async void PlayerChargeAttack()
+    public virtual void PlayerChargeAttack()
     {
-        await Task.Delay(_chargeAttackDelay);
-        //_wasCharge = false;
+
     }
 
     /// <summary>ボム使用時の処理</summary>
-    public virtual async void Bom()
+    public virtual void Bom()
     {
+        _isBomb = true;
         Debug.Log("ボム撃ったよー");
         Play(_playerBombShotAudio);
-        await Task.Delay(_bombCoolTime);
-        _isBomb = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -275,6 +286,7 @@ public class PlayerBase : MonoBehaviour
             {
                 Debug.LogError("おめぇーの残機ねえから！" + PlayerResidue);
                 GameManager.Instance.GameOver();
+                gameObject.SetActive(false);
             }
         }
 
@@ -323,7 +335,16 @@ public class PlayerBase : MonoBehaviour
         _godMode = true;
         _isNotControll = true;
         //_anim = _dead;
-        //_anim.Play();
+        _anim.Play();
+        //var sequence = DOTween.Sequence(DOTween.To(() => _color,
+        //    (x) =>
+        //    {
+        //        Color c = _color;
+        //        c.a = x;
+        //        _color = c;
+        //    },
+        //    _color,
+        //    _changeValueInterval));
         await Task.Delay(_respawnTime);
         _dir = Vector2.zero;
         transform.position = _playerRespawn.position;//ここでリスポーン地点に移動
