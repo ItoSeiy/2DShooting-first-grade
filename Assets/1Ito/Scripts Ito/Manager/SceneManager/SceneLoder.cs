a16ea74cef8c0ebaca148dd7adec567274095022
伊東聖矢 <tvedaamamesab@gmail.com>
2022-02-08 18:06:19 +0900

    フェイドマネージャ追加

---
+++ Assets/1Ito/Scripts Ito/Manager/SceneManager/SceneLoder.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// シングルトンパターンのシーンを遷移させるスクリプト
+/// </summary>
+public class SceneLoder : SingletonMonoBehaviour<SceneLoder>
+{
+    [SerializeField] Image _fadeImage = null;
+    [SerializeField] float _fadeTime = 1;
+    float _timer = 0;
+    bool _isLoad = false;
+    /// <summary>
+    /// シーンをロードする関数
+    /// </summary>
+    /// <param name="sceneName">遷移したいシーンの名前</param>
+    public void LoadScene (string sceneName)
+    {
+        if (_isLoad) return;
+        StartCoroutine(Fade(sceneName));
+    }
+
+    IEnumerator Fade(string sceneName)
+    {
+        _isLoad = true;
+        Color c;
+        while (_fadeImage.color.a < 1)
+        {
+            c = _fadeImage.color;
+            _timer += Time.deltaTime;
+            c.a = _timer / _fadeTime;
+            _fadeImage.color = c;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return SceneManager.LoadSceneAsync(sceneName);
+
+        while (_fadeImage.color.a >  0)
+        {
+            c = _fadeImage.color;
+            _timer -= Time.deltaTime;
+            c.a = _timer / _fadeTime;
+            c.a =  _timer / _fadeTime;
+            _fadeImage.color = c;
+            yield return new WaitForEndOfFrame();
+        }
+        _isLoad = false;    
+    }
+}
