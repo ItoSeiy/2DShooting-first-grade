9e46042c130ad63836faf498e494d6191d2cd150
安達青 <sei.dance.dream@gmail.com>
2022-02-10 20:29:00 +0900

    ボス敵制作中

--- Assets/5Adachi/Scripts/BossSpecialAttack/TestSpecialAttack.cs
+++ Assets/5Adachi/Scripts/BossSpecialAttack/TestSpecialAttack.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class TestSpecialAttack : EnemyBese
 {
     /// <summary>バレットのプレハブ</summary>
-    [SerializeField] List<GameObject> _enemyBulletPrefab = new List<GameObject>();
-    /// <summary>バレットを発射する場所</summary>
-    [SerializeField] List<Transform> m_muzzles =new List<Transform>();
+    [SerializeField,Header("Bulletのプレハブ")] List<GameObject> _enemyBulletPrefab = new List<GameObject>();
+    /// <summary>バレットを発射するポジション</summary>
+    [SerializeField, Header("Bulletを発射するポジション")] Transform[] _muzzles = null;
     void Start()
     {
-        
+        if (_muzzles == null || _muzzles.Length == 0)
+        {
+            _muzzles = new Transform[1] { this.transform };
+        }
     }
     protected override void Update()
     {
         base.Update();
     }
-
-   
+ 
     protected override void Attack()
+    {
+        //throw new System.NotImplementedException();
+
+        foreach (Transform t in _muzzles)
+        {
+            Instantiate(_enemyBulletPrefab[0], t.position, Quaternion.identity);
+        }
+    }
+
+    protected override void OnGetDamage()
     {
         throw new System.NotImplementedException();
     }
-
-
 }
