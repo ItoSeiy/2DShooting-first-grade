c2ab971f7b21e1ccd88f3c14a642ba7dd22d6eb2
神原琉成 <tekunikaru22@gmail.com>
2022-02-10 14:51:48 +0900

    ItemBase作業中

--- Assets/2Kanbara/Scripts Kanbara/Item/ItemBase.cs
+++ Assets/2Kanbara/Scripts Kanbara/Item/ItemBase.cs
@@ -8,13 +8,24 @@ using UnityEngine;
 public class ItemBase : MonoBehaviour
 {
     [SerializeField, Header("プレイヤーのタグ")] string _playerTag = "Player";
-
+    [SerializeField, Header("プレイヤーの持つアイテム回収用のコライダー")] string _playerTriggerTag = "PlayerTrigger";
+    GameObject _player;
+    Rigidbody2D _rb;
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == _playerTag)
         {
             Destroy(this.gameObject);
         }
+        if(collision.tag == _playerTriggerTag)
+        {
+            ApproachPlayer();
+        }
+    }
+
+    private void OnEnable()
+    {
+        _player = GameObject.Find("Player").GetComponent<GameObject>();
     }
 
     protected virtual void OnBecameInvisible()
@@ -22,8 +33,8 @@ public class ItemBase : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    protected virtual void UsingItem()
+    protected virtual void ApproachPlayer()
     {
-
+        _rb.velocity = this.gameObject.transform.position - _player.transform.position;
     }
 }
