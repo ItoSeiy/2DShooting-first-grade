bec74cab909f12b1a5ea873bb7558349f1a2c9dd
神原琉成 <tekunikaru22@gmail.com>
2022-02-16 20:07:44 +0900

    オプション作業中

---
+++ Assets/2Kanbara/Scripts Kanbara/KeyConfig/KeyConfig.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class KeyConfig : MonoBehaviour
+{
+    [SerializeField] 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
