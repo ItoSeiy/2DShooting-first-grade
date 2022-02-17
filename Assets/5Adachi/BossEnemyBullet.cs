f853d2bbcb680d79ae0b0a931a483e9698f81066
安達青 <sei.dance.dream@gmail.com>
2022-02-17 11:13:12 +0900

    ボス敵制作中

--- Assets/5Adachi/BossEnemyBullet.cs
+++ Assets/5Adachi/BossEnemyBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMove : BulletBese
+public class BossEnemyBullet : BulletBese
 {
     protected override void BulletMove()
     {
