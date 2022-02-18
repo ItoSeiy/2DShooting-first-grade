ae7d1877ca4340459ce39d8c2f4c3f724ea59d94
伊東聖矢 <tvedaamamesab@gmail.com>
2022-02-18 13:31:41 +0900

    ボスコントローラーwip

--- Assets/1Ito/Scripts Ito/Boss/BossEnemyController.cs
+++ Assets/1Ito/Scripts Ito/Boss/BossEnemyController.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class BossEnemyController : EnemyBase
 {
+    /// <summary> 敵の行動データ </summary>
+    public BossData Data => _data;
+    /// <summary> アニメーター </summary>
+    public Animator Animator { get; private set; } = default;
+
     [SerializeField]
     Transform[] _muzzles = null;
+    /// <summary> ボス行動データ </summary>
+    [SerializeField]
+    BossData _data = null;
 
+    /// <summary> 現在の行動 </summary>
+    private BossAction _currentBossAction = default;
+    /// <summary> 最後に行った行動パターンインデックス </summary>
+    private int _lastPattern = -1;
+    /// <summary> プレイヤー参照本体 </summary>
+    private GameObject _player = default;
+    /// <summary> 敵のモデル </summary>
+    public GameObject Model { get => gameObject; }
 
+    public GameObject PlayerObj
+    {
+        get
+        {
+            if (_player == null)
+            {
+                Debug.LogWarning("playerタグを持ったオブジェクトがありません。\n追加してください");
+                return null;
+            }
+            return _player;
+        }
+    }
 
     protected override void Attack()
     {
