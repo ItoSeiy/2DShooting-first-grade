0c60066d5ddc3f88ca820ba62854eefddc4807c2
近藤　倫太 <tora_goraias@gmail.com>
2022-02-18 18:54:02 +0900

    ステージセレクト画面とゲーム終了ボタンを実装しました

---
+++ Assets/6Kondo/Scripts/QuitGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void End()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+
+#endif
+    }
+}
