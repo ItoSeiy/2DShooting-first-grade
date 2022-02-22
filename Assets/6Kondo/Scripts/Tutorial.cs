d1d2fbe960837a2e2536c2c89125dff526d10d0b
近藤　倫太 <tora_goraias@gmail.com>
2022-02-22 13:32:25 +0900

    TutorialStagewip

--- Assets/6Kondo/Scripts/Tutorial.cs
+++ Assets/6Kondo/Scripts/Tutorial.cs
@@ -6,26 +6,52 @@ using UnityEngine.InputSystem;
 
 public class Tutorial : MonoBehaviour
 {
-    [SerializeField] GameObject _checkMove;
-    [SerializeField] InputAction _input;
+    [SerializeField, Header("移動チュートリアルのチェック")] GameObject _moveCheck;
+    [SerializeField, Header("移動チュートリアルの対応キー")] InputAction _inputMove;
+    [SerializeField, Header("射撃チュートリアルのチェック")] GameObject _shotCheck;
+    [SerializeField, Header("射撃チュートリアルの対応キー")] InputAction _inputShot;
+    [SerializeField, Header("精密操作のチュートリアルのチェック")] GameObject _slowMoveCheck;
+    [SerializeField, Header("精密操作のチュートリアルの対応キー")] InputAction _inputSlowMove;
     private void Start()
     {
         MoveTutorial();
+        ShotTutorial();
+        SlowMoveTutorial();
     }
     private void OnEnable()
     {
-        _input.Enable();
+        _inputMove.Enable();
+        _inputShot.Enable();
+        _inputSlowMove.Enable();
     }
     private void OnDisable()
     {
-        _input.Disable();
+        _inputMove.Disable();
+        _inputShot.Disable();
+        _inputSlowMove.Disable();
     }
-    private void SetCheck()
+    private void SetMoveCheck()
     {
-        _checkMove.SetActive(true);
+        _moveCheck.SetActive(true);
     }
     private void MoveTutorial()
     {            
-        _input.performed += _ => SetCheck();
+        _inputMove.performed += _ => SetMoveCheck();
+    }
+    private void SetShotCheck()
+    {
+        _shotCheck.SetActive(true);
+    }
+    private void ShotTutorial()
+    {
+        _inputShot.performed += _ => SetShotCheck();
+    }
+    private void SetSlowMoveCheck()
+    {
+        _slowMoveCheck.SetActive(true);
+    }
+    private void SlowMoveTutorial()
+    {
+        _inputShot.performed += _ => SetSlowMoveCheck();
     }
 }
