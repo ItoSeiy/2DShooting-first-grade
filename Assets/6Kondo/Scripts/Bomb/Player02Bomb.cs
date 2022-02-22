9979f049d0186185d4b29e6b5a144e34b8609b30
伊東聖矢 <tvedaamamesab@gmail.com>
2022-02-22 22:48:42 +0900

    ステージをセッティング中 神原のアイテム、近藤のUI, 和田のモブ敵、オブジェトプールをいじった アイテムを敵がドロップするようにもした

--- Assets/6Kondo/Scripts/Bomb/Player02Bomb.cs
+++ Assets/6Kondo/Scripts/Bomb/Player02Bomb.cs
@@ -17,7 +17,7 @@ public class Player02Bomb : BulletBese
     {
         for (int i = 0; i < _muzzle.Length; i++)
         {
-            var bombChild = ObjectPool.Instance.UseBullet(_muzzle[i].position, PoolObjectType.Player02BombChild);
+            var bombChild = ObjectPool.Instance.UseObject(_muzzle[i].position, PoolObjectType.Player02BombChild);
             bombChild.transform.rotation = _muzzle[i].rotation;
             yield return new WaitForSeconds(_childBulletDelay);
         }
