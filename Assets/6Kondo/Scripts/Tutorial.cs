026e7eda8094ef2871d1d4dc84f605de1c3e3468
近藤　倫太 <tora_goraias@gmail.com>
2022-02-22 17:04:45 +0900

    Tutorialwip

--- Assets/6Kondo/Scripts/Tutorial.cs
+++ Assets/6Kondo/Scripts/Tutorial.cs
@@ -8,8 +8,10 @@ public class Tutorial : MonoBehaviour
 {
     [SerializeField, Header("移動チュートリアルのチェック")] GameObject _moveCheck;
     [SerializeField, Header("移動チュートリアルの対応キー")] InputAction _inputMove;
-    [SerializeField, Header("射撃チュートリアルのチェック")] GameObject _shotCheck;
-    [SerializeField, Header("射撃チュートリアルの対応キー")] InputAction _inputShot;
+    [SerializeField, Header("ショットチュートリアルのチェック")] GameObject _shotCheck;
+    [SerializeField, Header("ショットチュートリアルの対応キー")] InputAction _inputShot;
+    [SerializeField, Header("チャージショットチュートリアルのチェック")] GameObject _chargeShotCheck;
+    [SerializeField, Header("チャージショットチュートリアルの対応キー")] InputAction _inputChargeShot;
     [SerializeField, Header("精密操作のチュートリアルのチェック")] GameObject _slowMoveCheck;
     [SerializeField, Header("精密操作のチュートリアルの対応キー")] InputAction _inputSlowMove;
     private void Start()
@@ -17,18 +19,21 @@ public class Tutorial : MonoBehaviour
         MoveTutorial();
         ShotTutorial();
         SlowMoveTutorial();
+        CharegeShotTutorial();
     }
     private void OnEnable()
     {
         _inputMove.Enable();
         _inputShot.Enable();
         _inputSlowMove.Enable();
+        _inputChargeShot.Enable();
     }
     private void OnDisable()
     {
         _inputMove.Disable();
         _inputShot.Disable();
         _inputSlowMove.Disable();
+        _inputChargeShot.Disable();
     }
     private void SetMoveCheck()
     {
@@ -46,12 +51,20 @@ public class Tutorial : MonoBehaviour
     {
         _inputShot.performed += _ => SetShotCheck();
     }
+    private void SetChargeCheck()
+    {
+        _chargeShotCheck.SetActive(true);
+    }
+    private void CharegeShotTutorial()
+    {
+        _inputChargeShot.performed += _ => SetChargeCheck();
+    }
     private void SetSlowMoveCheck()
     {
         _slowMoveCheck.SetActive(true);
     }
     private void SlowMoveTutorial()
     {
-        _inputShot.performed += _ => SetSlowMoveCheck();
+        _inputSlowMove.performed += _ => SetSlowMoveCheck();
     }
 }
