b0bb7154539edb1ffdce3fdf9caa8dfe88641358
安達青 <sei.dance.dream@gmail.com>
2022-02-23 11:00:12 +0900

    ボス敵制作中

--- Assets/5Adachi/Scripts/BossNormalAttack/BossNormalAttack04.cs
+++ Assets/5Adachi/Scripts/BossNormalAttack/BossNormalAttack04.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class BossNormalAttack04 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>形状や大きさの概念を持った物質</summary>
+    Rigidbody2D _rb;
+    /// <summary>方向</summary>
+    Vector3 _dir;
+    /// <summary>プレイヤーのオブジェクト</summary>
+    private GameObject _player;
+    /// <summary>プレイヤーのタグ</summary>
+    [SerializeField, Header("playerのtag")] string _playerTag = null;
+    /// <summary>バレットを発射するポジション</summary>
+    [SerializeField, Header("Bulletを発射するポジション")] Transform[] _muzzles = null;
+    /// <summary>攻撃頻度</summary>
+    [SerializeField, Header("攻撃頻度(秒)")] private float _attackInterval = 0.64f;
+    /// <summary>発射する弾を設定できる</summary>
+    [SerializeField, Header("発射する弾の設定")] PoolObjectType _bullet;
+    /// <summary>マズルの角度間隔</summary>
+    [SerializeField, Header("マズルの角度間隔")] float _rotationInterval = 10f;
+    /// <summary>最小の回転値</summary>
+    const float MINIMUM_ROT_RANGE = 0f;
+    /// <summary>最大の回転値</summary>
+    const float MAXIMUM_ROT_RANGE = 360f;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _player = GameObject.FindGameObjectWithTag(_playerTag);
+        StartCoroutine(Attack());
     }
 
-    // Update is called once per frame
-    void Update()
+    //Attack関数に入れる通常攻撃
+    IEnumerator Attack()
     {
-        
+        while (true)
+        {
+            //ターゲット（プレイヤー）の方向を計算
+            _dir = (_player.transform.position - _muzzles[0].transform.position);
+            //ターゲット（プレイヤー）の方向に回転
+            _muzzles[0].transform.rotation = Quaternion.FromToRotation(Vector3.up, _dir);
+
+            Vector3 localAngle = _muzzles[0].localEulerAngles;
+
+            //親オブジェクトのマズル
+
+            //弾をマズル0の向きに合わせて弾を発射
+            ObjectPool.Instance.UseObject(_muzzles[0].position, _bullet).transform.rotation = _muzzles[0].rotation;
+
+            //_rotationIntervalで設定した角度間隔で全方位に発射する
+            for (float rotation = MINIMUM_ROT_RANGE + localAngle.z; rotation < MAXIMUM_ROT_RANGE + localAngle.z; rotation += _rotationInterval)
+            {
+                ///マズルを回転する///
+                //Vector3 localAngle = _muzzles[0].localEulerAngles;// ローカル座標を基準に取得
+                //角度を設定
+                localAngle.z = rotation;
+                _muzzles[0].localEulerAngles = localAngle;//回転する
+                //弾をマズルの向きに合わせて弾を発射
+                ObjectPool.Instance.UseObject(_muzzles[0].position, _bullet).transform.rotation = _muzzles[0].rotation;
+            }
+
+            yield return new WaitForSeconds(_attackInterval);
+        }
     }
 }
