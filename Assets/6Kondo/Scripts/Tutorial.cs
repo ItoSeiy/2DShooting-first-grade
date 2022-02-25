7cf6a914baaf31cf8e6a1c642bf630f945329b2a
近藤　倫太 <tora_goraias@gmail.com>
2022-02-25 22:04:33 +0900

    Tutorial修正

--- Assets/6Kondo/Scripts/Tutorial.cs
+++ Assets/6Kondo/Scripts/Tutorial.cs
@@ -10,10 +10,10 @@ public class Tutorial : SingletonMonoBehaviour<Tutorial>
     [SerializeField, Header("移動チュートリアルの対応キー")] InputAction _inputMove;
     [SerializeField, Header("ショットチュートリアルのチェック")] GameObject _shotCheck;
     [SerializeField, Header("ショットチュートリアルの対応キー")] InputAction _inputShot;
-    [SerializeField, Header("チャージショットチュートリアルのチェック")] GameObject _chargeShotCheck;
-    [SerializeField, Header("チャージショットチュートリアルの対応キー")] InputAction _inputChargeShot;
     [SerializeField, Header("精密操作のチュートリアルのチェック")] GameObject _slowMoveCheck;
     [SerializeField, Header("精密操作のチュートリアルの対応キー")] InputAction _inputSlowMove;
+    [SerializeField, Header("チャージショットチュートリアルのチェック")] GameObject _chargeShotCheck;
+    [SerializeField, Header("チャージショットチュートリアルの対応キー")] InputAction _inputChargeShot;
     [SerializeField, Header("アイテム取得のチュートリアルのチェック")] GameObject _itemCheck;
     [SerializeField, Header("ボム取得のチュートリアルのチェック")] GameObject _bombCheck;
     [SerializeField, Header("残機取得のチュートリアルのチェック")] GameObject _residueCheck;
@@ -60,7 +60,7 @@ public class Tutorial : SingletonMonoBehaviour<Tutorial>
     }
     private void CharegeShotTutorial()
     {
-        _inputChargeShot.performed += _ => 
+        _inputChargeShot .performed += _ => 
         {
             _chargeShotCheck.SetActive(true);
             CheckClear();
