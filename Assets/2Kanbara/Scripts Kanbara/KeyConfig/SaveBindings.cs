ac7287abc86cab43bf0c65f39b4ae26154052756
神原琉成 <tekunikaru22@gmail.com>
2022-02-26 20:32:53 +0900

    SettingScene作業中

---
+++ Assets/2Kanbara/Scripts Kanbara/KeyConfig/SaveBindings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SaveBindings : MonoBehaviour
+{
+    public InputActionAsset _actions;
+
+    public void OnEnable()
+    {
+        var rebinds = PlayerPrefs.GetString("rebinds");
+        if(!string.IsNullOrEmpty(rebinds))
+        {
+            //_actions.LoadBindingOverrideFromJson(rebinds);
+        }
+    }
+
+    public void OnDisable()
+    {
+        //var rebinds = _actions.SaveBindingOverridesAsJson();
+        //PlayerPrefs.SetString("rebinds",rebinds);
+    }
+}
