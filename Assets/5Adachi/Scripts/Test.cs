eda1202a32734ec2cfc56807603b41cee3e4f196
安達青 <sei.dance.dream@gmail.com>
2022-03-05 23:37:03 +0900

    ボス敵製作中

---
+++ Assets/5Adachi/Scripts/Test.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]　GameObject _particleObject = null;
+    public float _timer = 0; 
+    void Start()
+    {
+        StartCoroutine(A()); 
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+    }
+
+    IEnumerator A()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f/ 60f);
+            if(_timer >= 2f)
+            {
+                Instantiate(_particleObject, transform.position, Quaternion.identity); //パーティクル用ゲームオブジェクト生成
+                Destroy(gameObject);
+                break;
+            }
+            
+        }      
+    }   
+}
